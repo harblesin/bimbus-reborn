9cbac8ab940090885436175f80639e92af305603
harblesin <waltermelonson@gmail.com>
2024-08-22 20:36:18 -0700

    Updated

--- server/server.js
+++ server/server.js
@@ -9,60 +9,15 @@ const socketIO = require("socket.io");
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(express.json({ limit: '1mb' }));
 
-// app.use(express.static(path.join(__dirname, '../build')));
-// app.use(express.static(path.join(__dirname + "../public")));
-// app.use(express.static(path.join(__dirname + "../../../../../Desktop/")));
-// app.use(express.static(path.join(__dirname + "../../../../../Videos")))
-
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname + "../../../../../../srv/Pictures")));
-//     app.use(express.static(path.join(__dirname + "../../../../../../srv/Music")));
-//     app.use(express.static(path.join(__dirname + "../../../../../../srv/Neural")));
-
-// } else {
-//     app.use(express.static(path.join(__dirname + "../../../../../Pictures")));
-//     app.use(express.static(path.join(__dirname + "../../../../euralblend")));
-//     app.use(express.static(path.join(__dirname + "../../../../../Music")))
-
-// }
+app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname + "../public")));
 
 app.use(router);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 let server = app.listen(PORT, () => {
     console.log(`NODE server now on port ${PORT}`)
 });
-
-// const io = socketIO(server, { cors: { origin: "*" } });
-
-// io.on("connection", (socket) => {
-
-//     socket.on('updateAllLinks', (event) => {
-//         socket.emit("updateAllLinks", { msg: `Updating All Links` })
-//     });
-
-//     socket.on('refreshLinks', (event) => {
-//         socket.broadcast.emit("refreshLinks", { msg: `refresh` })
-//     });
-
-//     socket.on('changeNowPlaying', (event) => {
-//         io.emit("changeNowPlaying", { ...event })
-//     });
-
-//     socket.on("prevSong", event => {
-//         socket.broadcast.emit("prevSong", { ...event })
-//     });
-
-//     socket.on("nextSong", event => {
-//         socket.broadcast.emit("nextSong", { ...event })
-//     });
-
-//     socket.on("requestingSongInfo", event => {
-//         socket.broadcast.emit("requestingSongInfo", { ...event })
-//     });
-
-//     socket.on("sendingSongInfo", event => {
-//         socket.broadcast.emit("sendingSongInfo", { ...event })
-//     });
-
-//     module.exports = io;
-// });
