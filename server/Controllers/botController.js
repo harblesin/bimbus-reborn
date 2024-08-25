95d0172e153e861e2402bb58806d35ead2ca5d17
harblesin <waltermelonson@gmail.com>
2024-08-24 21:12:24 -0700

    updted

--- server/Controllers/botController.js
+++ server/Controllers/botController.js
@@ -61,7 +61,7 @@ const addYoutubeLink = (req, res) => {
             }
             let linkInfo = await ytdl.getInfo(link);
             youtubeLinks.push({
-                id: linkInfo.player_response.videoDetails.title + 1,
+                id: youtubeLinks.length + 1,
                 title: linkInfo.player_response.videoDetails.title,
                 link: link,
                 image: linkInfo.player_response.videoDetails.thumbnail.thumbnails[0].url
