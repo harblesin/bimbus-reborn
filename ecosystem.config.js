36a08d03d071cb21fb81e70174b01b0d2cda568d
harblesin <waltermelonson@gmail.com>
2024-08-28 17:36:55 -0700

    Added ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: 'bimbus-reborn',         // Name of your application
+        script: 'server/server.ts',              // Entry point of your app (TypeScript file)
+        interpreter: 'ts-node',        // Specifies the interpreter to use
+        watch: true,                   // Optional: Watch files for changes and restart
+        env: {
+          NODE_ENV: 'development',    // Environment variables for development
+        },
+        env_production: {
+          NODE_ENV: 'production',     // Environment variables for production
+        }
+      }
+    ]
+  };
