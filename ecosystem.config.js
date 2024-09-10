1d8e1a942b8fae7cd9adcd345610e6f0e5fff3f9
harblesin <waltermelonson@gmail.com>
2024-09-10 03:54:14 -0700

    UPdated ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
       {
         name: 'bimbus-reborn',         // Name of your application
         script: 'main.ts',              // Entry point of your app (TypeScript file)
-        interpreter: '/home/yea/.nvm/versions/node/v20.5.0/bin/ts-node',        // Specifies the interpreter to use
+        interpreter: 'npx',
+        interpreter_args: 'ts-node',        // Specifies the interpreter to use
         watch: true,                   // Optional: Watch files for changes and restart
         env: {
           NODE_ENV: 'development',    // Environment variables for development
