1fd6ff8e4b0d679e9055da76d5e9dfa2262f1cb3
harblesin <waltermelonson@gmail.com>
2024-09-10 19:45:22 -0700

    M

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import startServer from "./server/server";
 
-startServer().catch(err => {
+startServer().catch((err: any) => {
     return console.log(`Error occurring starting http server: ${err}`);
 });
 
