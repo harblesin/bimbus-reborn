df1aed679dbe454c5f7f4e071b7404d88c18841b
harblesin <waltermelonson@gmail.com>
2024-09-10 19:20:14 -0700

    ts

--- server/server.ts
+++ server/server.ts
@@ -11,32 +11,32 @@ const PORT = process.env.NODE_SERVER_PORT;
 
 function startServer(): Promise<void> {
 
-    return new Promise<void>( async (resolve, reject) => {
-        try{
+    return new Promise<void>(async (resolve, reject) => {
+        try {
 
             app.use(express.urlencoded({ extended: true, limit: '1mb' }));
             app.use(express.json({ limit: '1mb' }));
-            
+
             app.use(express.static(path.join(__dirname, '../build')));
             app.use(express.static(path.join(__dirname + "../public")));
-            
+
             app.use(router);
-            
+
             app.get('*', (req, res) => {
                 res.sendFile(path.join(__dirname, '../build', 'index.html'));
             });
-            
+
             let server = app.listen(PORT, () => {
                 const now = new Date();
                 console.info(`${process.pid} | ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} | Server | Express server now running on port ${PORT}`);
             });
             initSocket(server);
             resolve();
-        } catch (err) {
+        } catch (err: any) {
             console.log(`Error occurring starting http server: ${err}`);
             reject(err);
         }
-        
+
     })
 }
 
